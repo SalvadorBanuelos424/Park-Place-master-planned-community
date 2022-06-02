ae9decf9862df3e0a5e73b4d10b493b809845bc5
Capwonderbread <hectcast10@yahoo.com>
2022-06-01 17:48:43 -0700

    got signup to function

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,3 @@
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -49,5 +25,4 @@ async function signupFormHandler(event) {
   }
 }
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
