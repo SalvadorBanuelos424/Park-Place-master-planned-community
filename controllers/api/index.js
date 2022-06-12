0ed3a2243ab903f768391145c548f31ccc226618
KyleP <kyl3p3tty@gmail.com>
2022-06-11 20:37:34 -0700

    cleaned up files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const calendarRoutes = require('./calendarRoutes');
 const reservationRoutes = require('./reservationRoutes');
-const homeRoutes = require('./homeRoutes');
 const eventRoutes = require('./eventRoutes');
 
 router.use('/users', userRoutes);
 router.use('/calendar', calendarRoutes);
 router.use('/reservations', reservationRoutes);
-router.use('/homes', homeRoutes);
 router.use('/event', eventRoutes);
 
 module.exports = router;
